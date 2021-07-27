c0af5687321333025c21dadc35410272ef524ffc
sanju <sk079997@gmail.com>
2021-07-27 18:54:40 +0530

    responsive problem

--- static/js/share_link.js
+++ static/js/share_link.js
@@ -2,10 +2,10 @@ const id = sessionStorage.getItem("IdSession");
 let idcookie = document.getElementById('getId').innerHTML;
 var link_box = document.querySelector(".box");
 if(idcookie!=''){
-    link_box.innerHTML = "https://realfriendshipquiz.herokuapp.com<br>/api/users?id="+idcookie;
+    link_box.innerHTML = "https://realfriendshipquiz.herokuapp.com/api/users?id="+idcookie;
 }
 else{
-    link_box.innerHTML = "https://realfriendshipquiz.herokuapp.com<br>/api/users?id="+id;
+    link_box.innerHTML = "https://realfriendshipquiz.herokuapp.com/api/users?id="+id;
 }
 
 var copyLink = document.getElementById('copy_link_img');
@@ -17,8 +17,9 @@ copyLink.addEventListener('click',e=>{
 
 var shareLink = document.getElementById('share_link_img');
 shareLink.addEventListener('click',e=>{
+    console.log(link_box.innerHTML);
     window.open(
-        "whatsapp://send?text=" + link_box.innerHTML,
+        "whatsapp://send?text=" + link_box.innerHTML.value,
         // open in a new window.
         '_blank' 
     );
